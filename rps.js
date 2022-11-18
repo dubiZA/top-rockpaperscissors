99018ded8d21f1a3656405c61b2905e0c64fa931
dubiZA <darius@dubiza.io>
2022-11-18 06:03:24 -0700

    Create basic game resources
    This commit creates the index.html and rps.js files to get all the
    browser required files in place.
    
    Additionally, rps.js has a "skeleton" of the functions that will
    be used at this point to make the game work, based on the
    instructions and requirements set in the lesson assignment.

---
+++ rps.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+
+  return computerChoice;
+}
+
+function getPlayerChoice() {
+  return PlayerChoice;
+}
+
+function playRound(computerSelection, playerSelection) {
+
+  return roundWinner;
+}
+
+function game() {
+  let roundResult = playRound(getComputerChoice(), getPlayerChoice());
+
+
+}
