81ae19fcca942830bb77d736f8a9193929902646
dubiZA <darius@dubiza.io>
2022-12-02 19:58:42 -0700

    Changed all single quotes to double for consistency

--- rps.js
+++ rps.js
@@ -1,33 +1,33 @@
 function getComputerChoice() {
   switch (Math.floor((Math.random() * 3) + 1)) {
     case 1:
-      return 'rock';
+      return "rock";
       break;
     case 2:
-      return 'paper';
+      return "paper";
       break;
     case 3:
-      return 'scissors';
+      return "scissors";
       break;
     default:
-      console.log('Something terrible happened! The computer made an invalid choice...');
+      console.log("Something terrible happened! The computer made an invalid choice...");
       break;
   }
 }
 
 function getPlayerChoice() {
-  const playerChoice = prompt('What is your selection?').toLowerCase();
+  const playerChoice = prompt("What is your selection?").toLowerCase();
   return playerChoice;
 }
 
 function playRound(computerSelection, playerSelection) {
   console.log(`Computer played: ${computerSelection}`)
-  if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')) {
-    return 'lose';
+  if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper") || (computerSelection === "paper" && playerSelection === "rock")) {
+    return "lose";
   } else if (computerSelection === playerSelection) {
-    return 'draw'
+    return "draw"
   } else {
-    return 'win';
+    return "win";
   }
 }
 
@@ -38,16 +38,16 @@ function game() {
   for (let i = 0; i < 5; i++) {
     let roundResult = playRound(getComputerChoice(), getPlayerChoice());
     switch (roundResult) {
-      case 'win':
-        console.log('You win this round!');
+      case "win":
+        console.log("You win this round!");
         playerScore++
         break;
-      case 'lose':
-        console.log('You lose this round!');
+      case "lose":
+        console.log("You lose this round!");
         computerScore++
         break;
-      case 'draw':
-        console.log('This round was a draw!');
+      case "draw":
+        console.log("This round was a draw!");
         break;
     }
   }
